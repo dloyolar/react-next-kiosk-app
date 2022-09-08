5a83bf091c25f10f12e113f3b8f60af7cb919d8c
dloyolar <dieegolr@gmail.com>
2022-09-08 19:06:42 -0300

    feat: create Spinner component

--- components/ui/Sidebar.jsx
+++ components/ui/Sidebar.jsx
@@ -10,12 +10,6 @@ export const Sidebar = () => {
     <>
       <Image width={300} height={100} src="/assets/img/logo.svg" alt="logo" />
 
-      {isLoading && (
-        <div className="flex h-[50%] items-center justify-center">
-          <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-amber-600"></div>
-        </div>
-      )}
-
       <nav className="mt-10">
         {categories.map((category) => (
           <Category key={category.id} category={category} />
---
+++ components/ui/Spinner.jsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="flex h-[50%] items-center justify-center">
+      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-amber-600"></div>
+    </div>
+  )
+}
