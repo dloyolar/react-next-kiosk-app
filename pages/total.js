cd96f293d7f9ea5b66898cfdf3918c25bf96112b
dloyolar <dieegolr@gmail.com>
2022-09-10 13:39:29 -0300

    fix: typo in total page

--- pages/total.js
+++ pages/total.js
@@ -16,7 +16,7 @@ const TotalPage = () => {
 
   return (
     <Layout page="Total">
-      <h1 className="text-4xl font-black">Daotos y Confirmar Pedido</h1>
+      <h1 className="text-4xl font-black">Datos y Confirmar Pedido</h1>
       <p className="my-10 text-2xl">Confirma tu pedido a continuación</p>
 
       <form onSubmit={submitOrder}>
