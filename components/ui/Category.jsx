a9450761c3589d2d013831510698014fb0c146bc
dloyolar <dieegolr@gmail.com>
2022-09-10 12:35:28 -0300

    fix: move to menu from other step

--- components/ui/Category.jsx
+++ components/ui/Category.jsx
@@ -1,16 +1,26 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { useContext } from 'react'
 import { KioskContext } from '../../context/KioskProvider'
 
 export const Category = ({ category }) => {
+  const router = useRouter()
   const { currentCategory, handleClickCategory } = useContext(KioskContext)
   const { name, icon, id } = category
+
+  const toHome = () => {
+    if (router.pathname !== '/') router.push('/')
+  }
+
   return (
     <div
       className={`${
         currentCategory?.id === id && 'bg-amber-400'
       } flex w-full items-center gap-4 border p-5 hover:cursor-pointer hover:bg-amber-400`}
-      onClick={() => handleClickCategory(id)}
+      onClick={() => {
+        handleClickCategory(id)
+        toHome()
+      }}
     >
       <Image
         alt="Imagen Icono"
